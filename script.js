3705a2d3b54809e6239d3aa84f80401dd7cea272
blackm5 <125949105+blackm5@users.noreply.github.com>
2024-08-12 03:59:57 -0300

    Finalização da clcomp

--- script.js
+++ script.js
@@ -3,19 +3,49 @@ let vm = window.document.querySelector('input#vmna');
 let tj = window.document.querySelector('input#tjna');
 let pna = window.document.querySelector('input#pna');
 let one = window.document.querySelector('article#sone');
-
-var start = window.document.querySelector('input#star')
-start.addEventListener('click', clic)
+var start = window.document.querySelector('input#star');
+start.addEventListener('click', clic);
 function clic() {
-    
         one.innerHTML = ''
         var o = document.createElement('p');
         o.innerHTML = 
         `
-            Valor Final: <strong>${((Number(vi.value)+(Number(vm.value)*/**/((((1+(Number(tj.value)/100))**Number(pna.value))-1)/(Number(tj.value)/100))*((1+(Number(tj.value)/100))**1)))).toFixed(2).replace('.',',')}$</strong><br>
-            Valor investido: <strong>${((Number(vi.value)+(Number(vm.value)*Number(pna.value)))).toFixed(2).replace('.',',')}$</strong><br>
-            Total em Juros: <strong>${((((Number(vi.value)+(Number(vm.value)*/**/((((1+(Number(tj.value)/100))**Number(pna.value))-1)/(Number(tj.value)/100))*((1+(Number(tj.value)/100))**1))))-(Number(vi.value)+(Number(vm.value)*Number(pna.value))))).toFixed(2).replace('.',',')}$</strong><br>
+        <span id="vf">
+            Valor Final: 
+                <strong>
+                    ${((Number(vi.value)*((1+(Number(tj.value)/100))**Number(pna.value)))+
+                    /*Aportes Mensais*/
+                    ((Number(vm.value)*(((1+(Number(tj.value)/100))**Number(pna.value))-1))/(Number(tj.value)/100))
+                    ).toFixed(2).replace('.',',')}$
+                </strong><br>
+        </span>
+        <span id="ti">
+            Valor investido: 
+                <strong>
+                    ${((Number(vi.value)+(Number(vm.value)*Number(pna.value)))).toFixed(2).replace('.',',')}$
+                </strong><br>
+        </span>
+        <span id="tj">
+            Total em Juros: 
+                <strong>
+                    ${
+                    (((Number(vi.value)*((1+(Number(tj.value)/100))**Number(pna.value)))+
+                    /*Aportes Mensais*/
+                    ((Number(vm.value)*(((1+(Number(tj.value)/100))**Number(pna.value))-1))/(Number(tj.value)/100))
+                    )-((Number(vi.value)+(Number(vm.value)*Number(pna.value))))).toFixed(2).replace('.',',')}
+                </strong><br>
+        </span>
         `
         one.appendChild(o)
-    
+}
+let an = window.document.querySelector('input#ano');
+var cha = window.document.querySelector('input#cal');
+let concl = window.document.querySelector('aside#con');
+cha.addEventListener('click', cli);
+function cli() {
+    concl.innerHTML = ''
+    var c = document.createElement('p');
+    c.innerHTML = 
+        `${Number(an.value)*12}`
+    concl.appendChild(c)
 }
